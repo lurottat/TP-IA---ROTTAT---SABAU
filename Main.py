257b16018d41f4362e0381d792fc97e2405728f3
Lucasrottat <47258401+Lucasrottat@users.noreply.github.com>
2019-11-16 17:29:11 +0100

    Push de la loose

--- Main.py
+++ Main.py
@@ -1,2 +1,20 @@
 import numpy as np
 
+def DocumentQuestion(FichierQuestion): 
+    Questions = FichierQuestion.readlines()
+    print(Questions)    
+    FichierQuestion.close()
+    
+def DocumentPersonnage(FichierPersonnage):
+    Personnages = FichierPersonnage.readlines()
+    '''[i.split('|',1)[0] for i in Personnages]
+    print(Personnages)
+    FichierPersonnage.close()'''
+    
+
+FichierQuestion = open("Question.txt", "r")  
+DocumentQuestion(FichierQuestion)
+
+FichierPersonnage = open("Personnage.txt", "r")  
+DocumentPersonnage(FichierPersonnage)
+
